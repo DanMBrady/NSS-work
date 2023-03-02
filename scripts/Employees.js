81dd35a979839c90787129bd2abb722ce64a5f74
Dan Brady <dannymb360@gmail.com>
2023-03-02 11:42:17 -0500

    initial commit

---
+++ scripts/Employees.js
@@ -0,0 +1,45 @@
+import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+
+const employees = getEmployees()
+const orders = getOrders()
+
+export const Employees = () => {
+    let html = "<ul>"
+
+    for (const employee of employees) {
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+    const itemClicked = clickEvent.target
+    if(itemClicked.id.startsWith("employee")){
+
+        const[, employeeId] = itemClicked.id.split("--")
+        
+        let correctEmployee = null
+        for(const employee of employees){
+            if(employee.id === parseInt(employeeId)){
+                correctEmployee = employee
+            }
+        }
+
+        let totalSales = 0
+        for(const order of orders){
+            if(order.employeeId === correctEmployee.id){
+                totalSales += 1
+            }
+        }
+        window.alert(`${correctEmployee.name} sold ${totalSales} products`)
+    }
+    
+}
+)
